b1268dc0a63f6a1b0967ed9b948660a87e130af6
Nicolas Bouton <nicolas.bouton@ens.uvsq.fr>
2019-11-23 16:02:15 +0100

    suppr

--- TD6/exo1a.lisp
+++
@@ -1,9 +0,0 @@
-(defun consec (l)
-	(cond
-		((null l) t)
-		((atom l) "error")
-		((and (and (listp l) (listp (cdr l)) (numberp (car l)) (numberp (car (cdr l)))) (and(< (- (car l)
-		(car (cdr l))) 2) (< (- (car (cdr l)) (car l) ) 2))) (consec (cdr l)))
-		(t nil)
-	)
-)
